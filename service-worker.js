e2ce6ee9e6c64c8cdc7bacabc9ee972ce7f94f81
Misaka16172 <uniballpete@outlook.com>
2025-12-27 19:28:27 +0800

    Site updated: 2025-12-27 19:28:27

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/css/page.css","2fde7279adc97cefd5399444158ac03a"],["/css/style.min.css","16a4cc20ac2ecce2dfd8bc11dcf43592"],["/css/vue-pdf.css","87f334c6ccfc3a397bf773e2f22a6767"],["/index.html","b820f847aa816bfc74184137f5df83c4"],["/js/common.js","ae798d30af603c3cbf2fa6549151dd0b"],["/js/css.escape.js","fe4db48c9e3f272a6d12cf1312de889e"],["/js/smooth-scroll.min.js","53a7fcc785e987d5ed08302f36de6653"],["/zh-cn/index.html","a41aa40eead604c59b7ca94b265a70c8"]];
+var precacheConfig = [["/css/page.css","2fde7279adc97cefd5399444158ac03a"],["/css/style.min.css","16a4cc20ac2ecce2dfd8bc11dcf43592"],["/css/vue-pdf.css","87f334c6ccfc3a397bf773e2f22a6767"],["/index.html","797e5c294a65a165be736aa022255e0f"],["/js/common.js","ae798d30af603c3cbf2fa6549151dd0b"],["/js/css.escape.js","fe4db48c9e3f272a6d12cf1312de889e"],["/js/smooth-scroll.min.js","53a7fcc785e987d5ed08302f36de6653"],["/zh-cn/index.html","a41aa40eead604c59b7ca94b265a70c8"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
